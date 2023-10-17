23c3e24f8fa8ae96b992ca8464549cf6b4e4f472
Ole Kristian Pedersen <okpedersen@gmail.com>
2023-10-17 19:35:46 +0200

    Initialize solutions with provider & workshop sandbox account id

---
+++ solutions/terraform.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.20.1"
+    }
+  }
+}
+
+provider "aws" {
+  # Must correspond to the AWS CLI configured profile name
+  profile = "ManagedAdministratorAccess-893160086441"
+  region = "eu-west-1"
+  allowed_account_ids = [ "893160086441" ]
+}
+
+data "aws_caller_identity" "current" {}
+
+output "user_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "user_id" {
+  value = data.aws_caller_identity.current.user_id
+}
