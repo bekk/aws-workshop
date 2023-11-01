a921a10d860bdd50cee505358c15179addf2a701
Ole Kristian Pedersen <okpedersen@gmail.com>
2023-11-01 01:01:14 +0100

    Add initial terraform.tf

---
+++ infra/terraform.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.20.1"
+    }
+  }
+}
+
+provider "aws" {
+  # Must correspond to the AWS CLI configured profile name
+  profile             = "cloudlabs"
+  region              = "eu-west-1"
+  allowed_account_ids = ["893160086441"]
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_caller_identity" "current" {}
+
+output "user_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "user_id" {
+  value = data.aws_caller_identity.current.user_id
+}
+
+# This check will display a warning to the participants if they forget to set
+# the id local variable in main.tf
+check "id_is_set" {
+  assert {
+    error_message = "Id must be set in main.tf"
+    condition     = length(local.id) > 0
+  }
+}
